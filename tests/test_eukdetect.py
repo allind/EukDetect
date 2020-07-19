d1a7ad676826fb989869d7dfa4e4dc8e35c0d5e1
Abigail Lind <alind@bueno.gladstone.internal>
2020-07-18 17:20:28 -0700

    update

--- tests/test_eukdetect.py
+++ tests/test_eukdetect.py
@@ -20,31 +20,25 @@ class _01_HelpText(unittest.TestCase):
 
 class _02_RunRunall(unittest.TestCase):
 	def test_class(self):
-		command = 'eukdetect --mode runall --configfile tests/test_se_configfile.yml --force'
+		command = 'eukdetect --mode runall --configfile tests/test_configfile.yml --force'
 		code, stdout, stderr = run(command)
 		self.assertTrue(code==0, msg=stderr)
 
 class _03_RunAln(unittest.TestCase):
 
 	def test_class(self):
-		command = 'eukdetect --mode aln --configfile tests/test_se_configfile.yml --force'
+		command = 'eukdetect --mode aln --configfile tests/test_configfile.yml --force'
 		code, stdout, stderr = run(command)
 		self.assertTrue(code==0, msg=stderr)
 
-class _04_RunAlncmd(unittest.TestCase):
+class _04_RunAln_and_filter(unittest.TestCase):
 
 	def test_class(self):
-		command = 'eukdetect --mode alncmd --configfile tests/test_se_configfile.yml --force'
-		code, stdout, stderr = run(command)
-		self.assertTrue(code==0, msg=stderr)
-
-
-class _05_RunFilter(unittest.TestCase):
-
-	def test_class(self):
-		command = 'eukdetect --mode filter --configfile tests/test_se_configfile.yml --force'
-		code, stdout, stderr = run(command)
-		self.assertTrue(code==0, msg=stderr)
+		command = 'eukdetect --mode alncmd --configfile tests/test_configfile.yml --force'
+		alncode, alnstdout, alnstderr = run(command)
+		command = 'eukdetect --mode filter --configfile tests/test_configfile.yml --force'
+		filtercode, filterstdout, filterstderr = run(command)
+		self.assertTrue(alncode==0 and filtercode == 0, msg=alnstderr + filterstderr)
 
 if __name__ == '__main__':
 	unittest.main()
